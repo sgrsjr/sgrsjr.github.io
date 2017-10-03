a383eb07f7be1a874a0ee477670b5f5f4373cc5f
Rameshwar Singh <sgrsjr@gmail.com>
2017-10-03 08:48:29 +0530

    Set Layout in details section(footer).

--- js/script.js
+++ js/script.js
@@ -55,21 +55,21 @@ function submitted() {
 }
 
 function renderHTML(data) {
-    document.getElementById("location").innerHTML = data.name + ', ' + data.sys.country;
+    document.getElementById("location").innerHTML = '<p>Location:</p>' + data.name + ', ' + data.sys.country;
     document.getElementById("weather-desc").innerHTML = data.weather[0].main;
     $("#weather-img").attr("data", 'img/weather-img/set-1/' + data.weather[0].icon + '.svg');
     document.getElementById("temp").innerHTML = data.main.temp + '°c';
-    document.getElementById("pressure").innerHTML = data.main.pressure + ' mBar';
-    document.getElementById("humidity").innerHTML = data.main.humidity + '%';
+    document.getElementById("pressure").innerHTML = '<p>Pressure:</p>' + data.main.pressure + ' mBar';
+    document.getElementById("humidity").innerHTML = '<p>Humidity:</p>' + data.main.humidity + '%';
     document.getElementById("temp-min").innerHTML = data.main.temp_min + '°c ↓';
     document.getElementById("temp-max").innerHTML = data.main.temp_max + '°c ↑';
-    document.getElementById("visibility").innerHTML = data.visibility / 1000 + ' km';
-    document.getElementById("wind-speed").innerHTML = ((data.wind.speed * 18) / 5).toFixed(2) + ' km/h';
-    document.getElementById("wind-deg").innerHTML = data.wind.deg + '°';
-    document.getElementById("clouds").innerHTML = data.clouds.all + '%';
+    document.getElementById("visibility").innerHTML = '<p>Visibility:</p>' + data.visibility / 1000 + ' km';
+    document.getElementById("wind-speed").innerHTML = '<p>Wind Speed:</p>' + ((data.wind.speed * 18) / 5).toFixed(2) + ' km/h';
+    document.getElementById("wind-deg").innerHTML = '<p>Wind Direction:</p>' + data.wind.deg + '°';
+    document.getElementById("clouds").innerHTML = '<p>Clouds:</p>' + data.clouds.all + '%';
     document.getElementById("date").innerHTML = getTime(data.dt).format1;
-    document.getElementById("sunrise").innerHTML = 'Sunrise ' + getTime(data.sys.sunrise).format_12 + ' AM';
-    document.getElementById("sunset").innerHTML = 'Sunset ' + getTime(data.sys.sunset).format_12 + ' PM';
+    document.getElementById("sunrise").innerHTML = '<p>Sunrise:</p>' + getTime(data.sys.sunrise).format_12 + ' AM';
+    document.getElementById("sunset").innerHTML = '<p>Sunset:</p>' + getTime(data.sys.sunset).format_12 + ' PM';
 
 }
 
